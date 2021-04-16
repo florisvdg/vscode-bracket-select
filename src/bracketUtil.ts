3715bd04b05f1efa32114426bdff96aa8114a0d1
Floris van der Grinten <floris@grinten.com>
2021-04-17 00:21:58 +0200

    Match less-than greater-than symbols as brackets

--- src/bracketUtil.ts
+++ src/bracketUtil.ts
@@ -4,7 +4,8 @@ export namespace bracketUtil {
     let bracketParis = [
         ["(", ")"], 
         ["{", "}"], 
-        ["[", "]"]
+        ["[", "]"],
+        ["<", ">"]
     ]
 
     let quoteBrackets = ['"', "'", "`"]
